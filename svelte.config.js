806ddc00bbdae51508b4015747fa94ce2be26d94
René Rafael <rrafael@groupsolver.com>
2023-01-19 21:17:55 +0100

    Updated packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,13 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true
-		})
-	],
-
-	kit: {
-		adapter: adapter()
-	}
+	preprocess: [vitePreprocess(), preprocess({ postcss: true })],
+	kit: { adapter: adapter({ edge: true }) }
 };
 
 export default config;
